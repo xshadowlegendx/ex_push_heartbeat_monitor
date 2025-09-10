9fe71e9895e3007cfa18822ec970aa229ba9d6bd
shadowlegend <lyheng0@gmail.com>
2025-09-10 23:35:49 +0700

    chore: remove warning + update tests

--- test/push_heartbeat_monitor_test.exs
+++ test/push_heartbeat_monitor_test.exs
@@ -1,28 +1,83 @@
 defmodule PushHeartbeatMonitorTest do
-  alias PushHeartbeatMonitorTest.HealthCheckMod
   use ExUnit.Case
   doctest PushHeartbeatMonitor
 
   import Mock
 
-  test "able to send push monitor at minimal config" do
+  test "able to start and send push monitor at minimal config" do
+    assert {:ok, state, {:continue, cont = :push_monitor}} = PushHeartbeatMonitor.init(%{
+      push_url: "https://uptime.acme.org/api/push/11111111-1111-1111-1111-111111111111",
+      push_method: :get,
+      push_success_arg: "status=up",
+      push_failure_arg: "status=down"
+    })
+
     with_mock Req, [
-      get: fn "https://uptime.acme.org/api/push/abcdef?status=up", [{:headers, []}] ->
+      get: fn _url, [{:headers, []}] ->
         {:ok, %Req.Response{status: 200}}
       end
     ] do
-      assert {:ok, _pid} = PushHeartbeatMonitor.start_link(%{
-        push_url: "https://uptime.acme.org/api/push/11111111-1111-1111-1111-111111111111",
-        push_method: :get,
-        push_success_arg: "status=up",
-        push_failure_arg: "status=down"
-      })
+      assert {:noreply, _state} = PushHeartbeatMonitor.handle_continue(cont, state)
+    end
+  end
+
+  test "able to handle error without crashing" do
+    with_mock Req, [
+      get: fn
+        "error-json:" <> _url, [{:headers, []}] ->
+          {:ok, %Req.Response{status: 500, body: "something went wrong"}}
+
+        "error-list:" <> _url, [{:headers, []}] ->
+          {:ok, %Req.Response{status: 500, body: [%{"detail" => nil, "error" => "something went wrong"}]}}
+
+        "error:" <> _url, [{:headers, []}] ->
+          {:error, %Req.TooManyRedirectsError{}}
+      end
+    ] do
+      assert {:noreply, _state} = PushHeartbeatMonitor.handle_info(
+        :push_monitor,
+        %{
+          config: %{
+            push_url: "error-json:https://uptime.acme.org/api/push/11111111-1111-1111-1111-111111111111",
+            push_interval: 32_000,
+            push_method: :get,
+            push_success_arg: "status=up",
+            push_failure_arg: "status=down"
+          }
+        }
+      )
+
+      assert {:noreply, _state} = PushHeartbeatMonitor.handle_info(
+        :push_monitor,
+        %{
+          config: %{
+            push_url: "error-list:https://uptime.acme.org/api/push/11111111-1111-1111-1111-111111111111",
+            push_interval: 32_000,
+            push_method: :get,
+            push_success_arg: "status=up",
+            push_failure_arg: "status=down"
+          }
+        }
+      )
+
+      assert {:noreply, _state} = PushHeartbeatMonitor.handle_info(
+        :push_monitor,
+        %{
+          config: %{
+            push_url: "error:https://uptime.acme.org/api/push/11111111-1111-1111-1111-111111111111",
+            push_interval: 32_000,
+            push_method: :get,
+            push_success_arg: "status=up",
+            push_failure_arg: "status=down"
+          }
+        }
+      )
     end
   end
 
   test "able to send push monitor with bearer token and healthcheck func" do
     with_mock Req, [
-      get: fn "https://gatus.acme.org/api/v1/endpoints/mygroup_myservice/external", [{:headers, [{"authorization", "Bearer 11111111-1111-1111-1111-111111111111"}]}] ->
+      post: fn "https://gatus.acme.org/api/v1/endpoints/mygroup_myservice/external?success=true", [{:headers, [{"authorization", "Bearer 11111111-1111-1111-1111-111111111111"}]}] ->
         {:ok, %Req.Response{status: 200}}
       end
     ] do
@@ -31,21 +86,28 @@ defmodule PushHeartbeatMonitorTest do
           do: :ok
       end
 
-      assert {:ok, _pid} = PushHeartbeatMonitor.start_link(%{
-        push_url: "https://gatus.acme.org/api/v1/endpoints/mygroup_myservice/external&success=true",
-        push_method: :post,
-        push_success_arg: "success=true",
-        push_failure_arg: "success=false",
-        push_token: "11111111-1111-1111-1111-111111111111",
-        push_token_placement: :bearer,
-        healthcheck_func: {HealthCheckModA, :success}
-      })
+      assert {:noreply, _state} =
+        PushHeartbeatMonitor.handle_info(
+          :push_monitor,
+          %{
+            config: %{
+              push_url: "https://gatus.acme.org/api/v1/endpoints/mygroup_myservice/external",
+              push_interval: 32_000,
+              push_method: :post,
+              push_success_arg: "success=true",
+              push_failure_arg: "success=false",
+              push_token: "11111111-1111-1111-1111-111111111111",
+              push_token_placement: :bearer,
+              healthcheck_func: {HealthCheckModA, :success}
+            }
+          }
+        )
     end
   end
 
   test "able to send push down status" do
     with_mock Req, [
-      get: fn "https://uptime.acme.org/api/push/abcdef?status=down&msg=something went wrong", [{:headers, [{"x-token", "abcdef"}]}] ->
+      get: fn "https://uptime.acme.org/api/push/11111111-1111-1111-1111-111111111111?status=down&msg=something went wrong", [{:headers, [{"x-token", "abcdef"}]}] ->
         {:ok, %Req.Response{status: 200}}
       end
     ] do
@@ -54,16 +116,23 @@ defmodule PushHeartbeatMonitorTest do
           do: {:error, "something went wrong"}
       end
 
-      assert {:ok, _pid} = PushHeartbeatMonitor.start_link(%{
-        push_url: "https://uptime.acme.org/api/push/11111111-1111-1111-1111-111111111111",
-        push_method: :get,
-        push_success_arg: "status=up",
-        push_failure_arg: "status=down",
-        push_message_param: "msg",
-        push_token: "abcdef",
-        healthcheck_func: {HealthCheckModB, :fail},
-        push_token_placement: {:header, "x-token"}
-      })
+      assert {:noreply, _state} =
+        PushHeartbeatMonitor.handle_info(
+          :push_monitor,
+          %{
+            config: %{
+              push_interval: 32_000,
+              push_url: "https://uptime.acme.org/api/push/11111111-1111-1111-1111-111111111111",
+              push_method: :get,
+              push_success_arg: "status=up",
+              push_failure_arg: "status=down",
+              push_message_param: "msg",
+              push_token: "abcdef",
+              healthcheck_func: {HealthCheckModB, :fail},
+              push_token_placement: {:header, "x-token"}
+            }
+          }
+        )
     end
   end
 end
